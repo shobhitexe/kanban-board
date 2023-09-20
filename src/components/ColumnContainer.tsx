5d2b815506721ce65b07c757b6da033e43fb9898
Sh0bhit <sguptamds94@gmail.com>
2023-09-20 19:15:57 +0530

    Added DND/Kit and built draggable columns

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,21 +1,92 @@
+import PlusIcon from "@/icons/PlusIcon";
 import TrashIcon from "@/icons/TrashIcon";
-import { ColumnsType } from "@/types";
+import { ColumnsType, IDType, TaskType } from "@/types";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { useState } from "react";
+import TaskCard from "./TaskCard";
 
 type ColumnContainerProps = {
   column: ColumnsType;
+  deleteColumn: (id: IDType) => void;
+  updateColumn: (id: IDType, title: string) => void;
+  createTask: (columnId: IDType) => void;
+  tasks: TaskType[];
+  deleteTask: (id: IDType) => void;
+  updateTask: (id: IDType, content: string) => void;
 };
 
-export default function ColumnContainer({ column }: ColumnContainerProps) {
+export default function ColumnContainer({
+  column,
+  deleteColumn,
+  updateColumn,
+  createTask,
+  tasks,
+  deleteTask,
+  updateTask,
+}: ColumnContainerProps) {
+  const [editMode, setEditMode] = useState<boolean>(false);
+
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: column.id,
+    data: {
+      type: "Column",
+      column,
+    },
+    disabled: editMode,
+  });
+
+  const style = { transition, transform: CSS.Transform.toString(transform) };
+
+  if (isDragging) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className=" bg-columnBackgroundColor w-[350px] h-[500px] max-h-[500px] flex flex-col"
+      ></div>
+    );
+  }
+
   return (
-    <div className=" bg-columnBackgroundColor w-[350px] h-[500px] max-h-[500px] flex flex-col">
-      <div className="flex gap-2 items-center w-full justify-center bg-mainBackgroundColor">
+    <div
+      ref={setNodeRef}
+      style={style}
+      className=" bg-columnBackgroundColor w-[350px] h-[500px] max-h-[500px] flex flex-col"
+    >
+      <div
+        {...attributes}
+        {...listeners}
+        onClick={() => setEditMode(true)}
+        className="flex gap-2 items-center w-full justify-center bg-mainBackgroundColor"
+      >
         <div className="flex justify-center px-2 py-1 text-sm rounded-full">
           0
         </div>
         <div className=" text-sm cursor-grab rounded-md rounded-b-none p-3 font-bold">
-          {column.title}
+          {!editMode && column.title}
+          {editMode && (
+            <input
+              value={column.title}
+              className="bg-black focus:border-rose-500 rounded outline-none px-2"
+              onChange={(e) => updateColumn(column.id, e.target.value)}
+              autoFocus
+              onBlur={() => setEditMode(false)}
+              onKeyDown={(e) => {
+                if (e.key !== "Enter") return;
+                setEditMode(false);
+              }}
+            ></input>
+          )}
         </div>
-        <button>
+        <button onClick={() => deleteColumn(column.id)}>
           <TrashIcon
             width={24}
             height={24}
@@ -24,8 +95,25 @@ export default function ColumnContainer({ column }: ColumnContainerProps) {
         </button>
       </div>
 
-      <div className="flex flex-grow">Content</div>
-      <div>Footer</div>
+      <div className="flex flex-grow flex-col gap-4 p-2 overflow-x-hidden overflow-y-auto">
+        {tasks.map((task) => (
+          <TaskCard
+            key={task.id}
+            task={task}
+            deleteTask={deleteTask}
+            updateTask={updateTask}
+          />
+        ))}
+      </div>
+      <button
+        onClick={() => {
+          createTask(column.id);
+        }}
+        className="flex gap-2 items-center border-columnBackgroundColor border-2 rounded-md p-4 hover:bg-mainBackgroundColor hover:text-rose-500 active:bg-black"
+      >
+        <PlusIcon width={20} height={20} />
+        Add Task
+      </button>
     </div>
   );
 }
--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,14 +1,32 @@
 "use client";
 
-import { useState } from "react";
-import { ColumnsType } from "../types";
+import { useMemo, useState, useEffect } from "react";
+import { ColumnsType, IDType, TaskType } from "../types";
 import PlusIcon from "@/icons/PlusIcon";
 import ColumnContainer from "./ColumnContainer";
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { SortableContext, arrayMove } from "@dnd-kit/sortable";
+import { createPortal } from "react-dom";
 
 export default function KanbanBoard() {
-  const [columns, setColumns] = useState<ColumnsType[]>([]);
+  const [mounted, setMounted] = useState<boolean>(false);
 
-  console.log(columns);
+  const [columns, setColumns] = useState<ColumnsType[]>([]);
+  const [activeColumn, setActiveColumn] = useState<ColumnsType | null>(null);
+
+  const [tasks, setTasks] = useState<TaskType[]>([]);
+
+  const columnsId = useMemo(() => columns.map((col) => col.id), [columns]);
+
+  useEffect(() => setMounted(true), []);
 
   function createNewColumn() {
     const columnsToAdd: ColumnsType = {
@@ -19,27 +37,139 @@ export default function KanbanBoard() {
     setColumns([...columns, columnsToAdd]);
   }
 
+  function deleteColumn(id: IDType) {
+    const filteredColumns = columns.filter((col) => col.id !== id);
+    setColumns(filteredColumns);
+  }
+
+  function updateColumn(id: IDType, title: string) {
+    const newColumns = columns.map((col) => {
+      if (col.id !== id) return col;
+      return { ...col, title };
+    });
+
+    setColumns(newColumns);
+  }
+
   function generateID() {
     return Math.floor(Math.random() * 10001);
   }
 
+  function onDragStart(event: DragStartEvent) {
+    if (event.active.data.current?.type === "Column") {
+      setActiveColumn(event.active.data.current.column);
+      return;
+    }
+  }
+
+  function onDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+
+    if (!over) return;
+
+    const activeColumnId = active.id;
+    const overColumnId = over.id;
+
+    if (activeColumnId === overColumnId) return;
+
+    setColumns((columns) => {
+      const activeColumnIndex = columns.findIndex(
+        (column) => column.id === activeColumnId
+      );
+
+      const overColumnIndex = columns.findIndex(
+        (column) => column.id === overColumnId
+      );
+
+      return arrayMove(columns, activeColumnIndex, overColumnIndex);
+    });
+  }
+
+  const sensor = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 3,
+      },
+    })
+  );
+
+  function createTask(columnId: IDType) {
+    const newTask: TaskType = {
+      id: generateID(),
+      columnId: columnId,
+      content: `Task ${tasks.length + 1}`,
+    };
+
+    setTasks([...tasks, newTask]);
+  }
+
+  function deleteTask(id: IDType) {
+    const newTasks = tasks.filter((task) => task.id !== id);
+    setTasks(newTasks);
+  }
+
+  function updateTask(id: IDType, content: string) {
+    const newTasks = tasks.map((task) => {
+      if (task.id !== id) return task;
+      return { ...task, content };
+    });
+
+    setTasks(newTasks);
+  }
+
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
-      <div className="am-auto flex gap-4">
-        <div className="flex gap-2">
-          {columns.map((column) => (
-            <ColumnContainer key={`${column.id}`} column={column} />
-          ))}
-        </div>
-      </div>
-
-      <button
-        onClick={() => createNewColumn()}
-        className="h-[60px] w-[350px] min-w-[350px] items-center cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2"
+      <DndContext
+        sensors={sensor}
+        onDragStart={onDragStart}
+        onDragEnd={onDragEnd}
       >
-        <PlusIcon width={20} height={20} />
-        Add Column
-      </button>
+        <div className="am-auto flex gap-4">
+          <div className="flex gap-2">
+            <SortableContext items={columnsId}>
+              {columns.map((column) => (
+                <ColumnContainer
+                  key={`${column.id}`}
+                  column={column}
+                  deleteColumn={deleteColumn}
+                  updateColumn={updateColumn}
+                  createTask={createTask}
+                  tasks={tasks.filter((task) => task.columnId === column.id)}
+                  deleteTask={deleteTask}
+                  updateTask={updateTask}
+                />
+              ))}
+            </SortableContext>
+          </div>
+          <button
+            onClick={() => createNewColumn()}
+            className="h-[60px] w-[350px] min-w-[350px] items-center cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2"
+          >
+            <PlusIcon width={20} height={20} />
+            Add Column
+          </button>{" "}
+        </div>
+        {mounted
+          ? createPortal(
+              <DragOverlay>
+                {activeColumn && (
+                  <ColumnContainer
+                    column={activeColumn}
+                    deleteColumn={deleteColumn}
+                    updateColumn={updateColumn}
+                    createTask={createTask}
+                    tasks={tasks.filter(
+                      (task) => task.columnId === activeColumn.id
+                    )}
+                    deleteTask={deleteTask}
+                    updateTask={updateTask}
+                  />
+                )}
+              </DragOverlay>,
+              document.body
+            )
+          : null}
+      </DndContext>
     </div>
   );
 }
