2e0df86d33e799080c1966d872d93fdcfe4d9c7d
Sh0bhit <sguptamds94@gmail.com>
2023-09-25 19:44:30 +0530

    introduced new order elemet to column obj

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 export type IDType = string | number;
 
-export type ColumnsType = { id: IDType; title: string };
+export type ColumnsType = { id: IDType; title: string; order: number };
 
 export type TaskType = { id: IDType; columnId: IDType; content: string };
